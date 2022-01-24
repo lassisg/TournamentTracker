ac3720c52c749db295a762991a4f447105f6c24d
Leandro Gabriel <leandro@bigiogabriel.com>
2022-01-24 14:24:30 +0000

    Added summary comments.

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -1,8 +1,28 @@
-﻿public class PrizeModel
+﻿/// <summary>
+/// Represents what the prize is for the given place.
+/// </summary>
+public class PrizeModel
 {
+    /// <summary>
+    /// The numeric identifier for the place (2 for second place,  etc.)
+    /// </summary>
     public int PlaceNumber { get; set; }
+
+    /// <summary>
+    /// The friendly name for the place (second place, first runner up, etc.)
+    /// </summary>
     public string PlaceName { get; set; }
+
+    /// <summary>
+    /// The fixed amount this place earns or zero if is not used.
+    /// </summary>
     public decimal PrizeAmount { get; set; }
+
+    /// <summary>
+    /// The number that represents the percentage of the overall take or
+    /// zero if it is not used. The percentage is the fraction of 1 (so 0.5 for
+    /// 50%).
+    /// </summary>
     public double PrizePercentage { get; set; }
 
 }
